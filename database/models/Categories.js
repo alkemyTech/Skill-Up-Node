50ad1e53ce480d96a5bf313a224b052e94983a19
Luis De La Espriella <luisdelaespriellaj@hotmail.com>
2022-11-04 14:38:21 -0500

    correcting the bug in model catedory (#11)

--- database/models/Categories.js
+++ database/models/Categories.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Categories.hasMany(models.Transactions,{
-        foreignKey:'categorityid'
+        foreignKey:'categoryId'
       })
     }
   };
