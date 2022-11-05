146cdabe905027b98895e17ada7019fee618ca2d
German Walton <waltongerman@gmail.com>
2022-11-05 00:43:41 -0300

    [ FIX ] Wa 19 clean code (#15)
    * validation middleware done
    
    * validation middleware done
    
    * removed repeated code

--- controllers/transactions.controllers.js
+++ controllers/transactions.controllers.js
@@ -1,35 +1,18 @@
-const createHttpError = require('http-errors');
-const { Transaction } = require('../database/models');
-const { endpointResponse } = require('../helpers/success');
-const { catchAsync } = require('../helpers/catchAsync');
+const createHttpError = require("http-errors");
+const { Transaction } = require("../database/models");
+const { endpointResponse } = require("../helpers/success");
+const { catchAsync } = require("../helpers/catchAsync");
 
 // example of a controller. First call the service, then build the controller method
 module.exports = {
   postCreateTransaction: catchAsync(async (req, res, next) => {
     try {
-      const { description, amount, date, userId, categoryId, user, category } =
-        req.body;
-
-      await Transaction.create({
-        description,
-        amount,
-        date,
-        userId,
-        categoryId,
-      });
-
-      const body = {
-        description,
-        amount,
-        date,
-        user,
-        category,
-      };
+      const response = await Transaction.create(req.body);
 
       endpointResponse({
         res,
-        message: 'Transaction created successfully.',
-        body,
+        message: "Transaction created successfully.",
+        body:response
       });
     } catch (error) {
       const httpError = createHttpError(
