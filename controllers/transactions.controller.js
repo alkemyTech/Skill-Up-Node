d0574a2780b1a6213ddfecd16ac94eca419d85bc
Luis De La Espriella <luisdelaespriellaj@hotmail.com>
2022-11-06 12:38:44 -0500

    [ENDPOINT] to edit Transactions (#28)
    * Endpoint to edit Transactions
    
    * adding schema and route validations
    
    * correcting the controller
    
    * add id in controller
    
    * correcting the schema
    
    * changing file locations
    
    * changes name of controller
    
    * Endpoint to edit Transactions
    
    * adding schema and route validations
    
    * correcting the controller
    
    * add id in controller
    
    * correcting the schema
    
    * changing file locations
    
    * changes name of controller
    
    * ending Endpoint to edit Transactions

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -1,13 +1,12 @@
 const createHttpError = require("http-errors");
-const { Transaction } = require("../database/models");
 const { endpointResponse } = require("../helpers/success");
 const { catchAsync } = require("../helpers/catchAsync");
+const { Transactions } = require("../database/models");
 
-// example of a controller. First call the service, then build the controller method
 module.exports = {
   postCreateTransaction: catchAsync(async (req, res, next) => {
     try {
-      const response = await Transaction.create(req.body);
+      const response = await Transactions.create(req.body);
 
       endpointResponse({
         res,
@@ -18,6 +17,25 @@ module.exports = {
       const httpError = createHttpError(
         error.statusCode,
         `[Error retrieving Transaction] - [Transaction - POST]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
+  put: catchAsync(async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const response = await Transactions.update(req.body, {
+        where: { id: `${id}` },
+      });
+      endpointResponse({
+        res,
+        message: "successfully",
+        body: req.body,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving transaction] - [transaction - PUT]: ${error.message}`
       );
       next(httpError);
     }
--- routes/transactions.router.js
+++ routes/transactions.router.js
@@ -4,8 +4,12 @@ const postTransactionSchema = require('../schemas/transaction/postTransactionSch
 const {
     validateRequestSchema,
   } = require('../middlewares/validation/validate-schema.middleware');
+const { put } = require('../controllers/transactions.controller')
+const putValidation  = require('../schemas/transaction/putTransactionShema');
+
 const router = express.Router();
 
+router.put('/:id', validateRequestSchema(putValidation), put);
 router.post('/', validateRequestSchema(postTransactionSchema), postCreateTransaction);
 
-module.exports = router;
+module.exports = router
