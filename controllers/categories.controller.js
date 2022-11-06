2a185aa37ef2a91fef35a7470f65da627ad7fb3c
HugoCavalero92 <72712498+HugoCavalero92@users.noreply.github.com>
2022-11-06 15:16:02 -0300

    [ENDPOINT] Endpoint for list Categories (#29)

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,7 +1,7 @@
 const createHttpError = require('http-errors')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
-const { Categories } = require('../database/models')
+const { Categories } = require('../database/models/')
 
 module.exports = {
   postCreateCategory: catchAsync(async (req, res, next) => {
@@ -17,7 +17,29 @@ module.exports = {
         error.statusCode,
         `[Error retrieving category] - [Category - POST]: ${error.message}`,
       )
-      next(httpError)
+      next(httpError);
     }
   }),
+
+  
+  getCategories: catchAsync(async (req, res, next) => {
+      try {
+        const response = await Categories.findAll()
+        endpointResponse({
+          res,
+          message: 'Test retrieved successfully',
+          body: response,
+        })
+      } catch (error) {
+        const httpError = createHttpError(
+          error.statusCode,
+          `[Error retrieving index] - [index - GET]: ${error.message}`,
+        )
+        next(httpError)
+      }
+    }),
+  
+
 }
+
+
