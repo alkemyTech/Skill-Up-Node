c53410b668e8c59ceb5738d638b9cedb98282025
Agustin Canteros <canterosagustindev@gmail.com>
2022-11-08 17:09:58 -0300

    .env example added

--- middlewares/token/jwt.middleware.js
+++ middlewares/token/jwt.middleware.js
@@ -1,25 +1,27 @@
-const jwt = require('jsonwebtoken');
-const {ErrorObject} = require('../../helpers/error')
-require('dotenv').config()
+const jwt = require("jsonwebtoken");
+const { ErrorObject } = require("../../helpers/error");
+require("dotenv").config();
 
 module.exports = {
-  decode: async function(token) {
-      const bearerToken = token.split(" ")[1]
-      const tokenDecode = await jwt.decode(bearerToken)
-      return tokenDecode
+  decode: async function (token) {
+    const bearerToken = token.split(" ")[1];
+    const tokenDecode = await jwt.decode(bearerToken);
+    return tokenDecode;
   },
-  encode: async function(payload) {
-    const token = await jwt.sign(payload, process.env.SECRET, { expiresIn: 86400 })
-    return token
+  encode: async function (payload) {
+    const token = await jwt.sign(payload, process.env.SECRET, {
+      expiresIn: 86400,
+    });
+    return token;
   },
-  verify: async function(req, res, next) {
+  verify: async function (req, res, next) {
     try {
-      const bearerToken = req.headers["authorization"].split(" ")[1]
-      await jwt.verify(bearerToken, process.env.SECRET)
-      next()
-    } catch(error){
-      const err = new ErrorObject(error, 401)
-      res.status(401).send(err)
+      const bearerToken = req.headers["authorization"].split(" ")[1];
+      await jwt.verify(bearerToken, process.env.SECRET);
+      next();
+    } catch (error) {
+      const err = new ErrorObject(error, 401);
+      res.status(401).send(err);
     }
-  }
-}
+  },
+};
