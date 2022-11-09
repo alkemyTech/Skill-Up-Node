1cc25b9485742e00137986a5db3cfd02b79d0edc
German Walton <waltongerman@gmail.com>
2022-11-09 00:22:03 -0300

    sprint1 (#46)
    * adding query validation (#44)
    
    * [ENDPOINT] authenticate users (#45)
    
    * wa-3 fix
    
    * autenticar user
    
    * Endpoint to authenticate users and correction fix
    
    * deleting files to merge
    
    * finishing sprint 1
    
    * correcting update
    
    Co-authored-by: jarriech <jarriech@gmail.com>
    
    Co-authored-by: Luis De La Espriella <luisdelaespriellaj@hotmail.com>
    Co-authored-by: jarriech <jarriech@gmail.com>
    Co-authored-by: Agustin Canteros <86705333+AgustinCanteros@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
-const { get } = require('../controllers/index')
 const transactionRoutes = require('./transactions.router');
 const usersRoutes = require('./users.router')
 const categoriesRoutes = require('./categories.router');
+const authRouter = require("./auth.router")
 
 const router = express.Router()
 
 // example of a route with index controller get function
 router.use('/transactions', transactionRoutes);
+router.use("/auth", authRouter);
 router.use('/users', usersRoutes)
 router.use('/categories', categoriesRoutes);
 
--- schemas/user/editUserSchema.js
+++ schemas/user/editUserSchema.js
@@ -2,14 +2,13 @@ const { Users } = require("../../database/models");
 const { ErrorObject } = require("../../helpers/error");
 
 module.exports = {
-  user: {
+  id: {
     custom: {
-      options: async (value, { req }) => {
-        const { id } = req.params;
-        const exist = await Users.findByPk(id);
-        if (!exist)
-          throw new ErrorObject({ message: "User not found", statusCode: 404 });
-      },
-    },
+      options: async (value, {req}) => {
+        const {id} = req.params
+        const exist = await Users.findByPk(id)
+        if(!exist) throw new ErrorObject('User not found',404)
+      }
+    }
   },
-};
+}
